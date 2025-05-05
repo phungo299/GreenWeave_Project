7258c58d40475f52268b668fb181cd59173da0da
AsanoKinoko <yuukiasuna13122002@gmail.com>
2025-05-05 21:20:01 +0700

    Refactor: Move AdminSidebar from ui to layout

--- frontend/src/components/layout/adminlayout/AdminLayout.jsx
+++ frontend/src/components/layout/adminlayout/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import AdminSidebar from '../../ui/adminsidebar/AdminSidebar';
+import AdminSidebar from '../adminsidebar/AdminSidebar';
 import './AdminLayout.css';
 
 const AdminLayout = () => {
