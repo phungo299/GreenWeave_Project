ffef093d2f374736676069dfa9234c23a02572a8
AsanoKinoko <yuukiasuna13122002@gmail.com>
2025-05-22 22:45:26 +0700

    Refactor: Uncomment on AdminRoute

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -5,15 +5,15 @@ import { useAuth } from '../context/AuthContext';
 const AdminRoute = () => {
     const { isAuthenticated, user } = useAuth();
     
-    // // Check if the user is logged in and has admin rights
-    // if (!isAuthenticated) {
-    //     return <Navigate to="/login" replace />;
-    // }
+    // Check if the user is logged in and has admin rights
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
     
-    // // Check if user has role admin
-    // if (user && user.role !== 'admin') {
-    //     return <Navigate to="/" replace />;
-    // }
+    // Check if user has role admin
+    if (user && user.role !== 'admin') {
+        return <Navigate to="/" replace />;
+    }
     
     // If user is admin, display child components
     return <Outlet />;
