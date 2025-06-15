cd8c0481274cdc0e26b62dc9d8a0838d9fc5ce2d
thepKz <maitanthepmrthep@gmail.com>
2025-06-16 01:09:47 +0700

    Fix: Fix auto

---
+++ backend/src/types/swagger-autogen.d.ts
@@ -0,0 +1,6 @@
+declare module 'swagger-autogen' {
+  // Minimal type declaration for swagger-autogen until official typings are provided
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const swaggerAutogen: (options?: any) => any;
+  export default swaggerAutogen;
+} 
